4b3ed6a6f700aff7de89238f0b38427b5031f1c4
sacrtap <89307033@qq.com>
2016-08-26 09:37:30 +0800

    Initial Commit

---
+++ vrapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vrapp
+//
+//  Created by 谭世涛 on 16/8/26.
+//  Copyright © 2016年 sam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
